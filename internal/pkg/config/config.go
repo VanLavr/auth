c2affba6e9a5d3740dc39881d3c5f87f5212c07c
VanLavr <lavr.i-n@mail.ru>
2024-03-30 14:00:25 +0300

    added New for config

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,6 +1,13 @@
 package config
 
-import "time"
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
 	Addr           string
@@ -12,4 +19,52 @@ type Config struct {
 	RefreshExpTime time.Duration
 	DBName         string
 	CollectionName string
+}
+
+func New() *Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+
+	read := os.Getenv("READTMT")
+	readtimeout, err := strconv.Atoi(read)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	write := os.Getenv("WRITETMT")
+	writetimeout, err := strconv.Atoi(write)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	maxbytes := os.Getenv("MAXB")
+	maxheaderbytes, err := strconv.Atoi(maxbytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	actime := os.Getenv("ACCESSTIME")
+	access, err := strconv.Atoi(actime)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reftime := os.Getenv("ACCESSTIME")
+	refresh, err := strconv.Atoi(reftime)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Config{
+		Addr:           os.Getenv("ADDR"),
+		Secret:         os.Getenv("SECRET"),
+		DBName:         os.Getenv("DBNAME"),
+		CollectionName: os.Getenv("COLLNAME"),
+		ReadTimeout:    time.Duration(readtimeout),
+		WriteTimeout:   time.Duration(writetimeout),
+		MaxHeaderBytes: maxheaderbytes,
+		AccessExpTime:  time.Duration(access),
+		RefreshExpTime: time.Duration(refresh),
+	}
 }
